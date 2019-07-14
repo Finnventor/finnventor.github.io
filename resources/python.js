207b72b80bcdc7c52e0d8bd55b35f6ccc9379955
Finnventor <50817575+Finnventor@users.noreply.github.com>
2019-07-14 13:47:28 -0700

    Updated pages

--- resources/python.js
+++ resources/python.js
@@ -7,8 +7,9 @@ function pipdialog(module) {
   try {
     dialog.showModal();
   } catch (e) {
-    alert("Paste the command 'pip install '"+module+"' into a terminal.");
+    alert("Paste the command 'pip install '"+module+"' into a terminal/command prompt.");
     console.warn("Warning: You are using a browser that does not support the dialog object. Defaulting to alert() message.");
     console.debug(e);
   }
+  return false;
 }
