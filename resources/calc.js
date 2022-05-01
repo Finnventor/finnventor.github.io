6e73ca7313d96d26bc9c26beb9c7596605b26707
Finnventor <50817575+Finnventor@users.noreply.github.com>
2022-04-30 23:23:07 -0700

    Update calc.js

--- resources/calc.js
+++ resources/calc.js
@@ -10,7 +10,7 @@ var usecookies = false;
 )*/
 
 function calc(x) {
-  var v = x.value.replace(/×|·|⋅|✕|✖|⨉|⨯/, '*').replace(/÷|∕|➗|⟌/, '/');
+  var v = x.value.replace(/˖|ᐩ|⁺|₊|➕/, '+').replace(/−|➖|⁻|₋|‐|‑|‒|–|—/, '-').replace(/×|·|⋅|✕|✖|⨉|⨯/, '*').replace(/÷|∕|➗|⟌/, '/');
   if (/^\s*$/.test(v)) {
     output.innerHTML = "<br/>";
     left_paren.innerHTML = "";
