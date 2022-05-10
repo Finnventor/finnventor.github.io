cda0555bf3cffcaa4ba865e66690509cf736e1e9
Finnventor <50817575+Finnventor@users.noreply.github.com>
2022-05-09 18:47:59 -0700

    Update calc.js

--- resources/calc.js
+++ resources/calc.js
@@ -114,7 +114,7 @@ function setconstants(parser) {
   parser.evaluate('NA=6.02214076e23/mol');
   parser.evaluate('q=1.602176634e19C');
   parser.evaluate('h=6.62607015e-34J/Hz');
-  parser.evaluate('hbar=1.054571817J*s');
+  parser.evaluate('hbar=1.054571817e-34J*s');
   parser.evaluate('lorentz(v)=1/sqrt(1-v^2/c^2)');
   parser.evaluate('alorentz(a)=sqrt(1-1/a^2)c');
 
