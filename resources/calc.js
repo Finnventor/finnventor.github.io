50762dbc5d0f638962e2eaf64d0fc0309b09a400
Finnventor <50817575+Finnventor@users.noreply.github.com>
2023-10-04 16:33:03 -0700

    Update calc.js

--- resources/calc.js
+++ resources/calc.js
@@ -55,7 +55,7 @@ function calc(x) {
         return;
       }
     }
-    output.innerHTML = out.replace(/\*/g, '×');
+    output.innerHTML = out.replace('*', '×').replace(' degC ', '°C').replace(' degF ', '°F');
   }
 }
 
